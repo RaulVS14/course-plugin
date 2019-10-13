3f2cfd9763f6876e62cbf25abb9d3ea29441c086
RawlM <raul.muser@gmail.com>
2019-10-13 15:41:39 +0300

    Add header for plugin file

---
+++ course-plugin.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Plugin Name:       Course Plugin
+ * Plugin URI:        http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            RawlM
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       course-plugin
+ * Domain Path:       /languages
+ */
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+ *
+ * 1. HOOKS
+ *
+ * 2. SHORTCODES
+ *
+ * 3. FILTERS
+ *
+ * 4. EXTERNAL SCRIPTS
+ *
+ * 5. ACTIONS
+ *
+ * 6. HELPERS
+ *
+ * 7. CUSTOM POST TYPES
+ *
+ * 8. ADMIN PAGES
+ *
+ * 9. SETTINGS
+ *
+ * 10. MISCELLANEOUS
+ *
+ */
+
+/* !1. HOOKS */
+
+
+
+/* !2. SHORTCODES */
+
+
+
+/* !3. FILTERS */
+
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+/* !5. ACTIONS */
+
+
+
+/* !6. HELPERS */
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+/* !10. MISCELLANEOUS */
+
+
